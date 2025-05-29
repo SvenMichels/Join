c2f8eb75e3288d1238e778fe6171751e16ddd7a9
Daniel Riedel <d.riedel.1@web.de>
2025-05-29 21:05:26 +0200

    addTask no grid best
    grid entfernt

--- addTask/addTask.js
+++ addTask/addTask.js
@@ -11,13 +11,13 @@
 //   });
 // }
 
-  const input = document.getElementById("input1");
-  const error = document.querySelector(".error-message");
+  // const input = document.getElementById("input1");
+  // const error = document.querySelector(".error-message");
 
-  input.addEventListener("input2", () => {
-    if (input.validity.valid) {
-      error.style.display = "none";
-    } else {
-      error.style.display = "block";
-    }
-  });
+  // input.addEventListener("input2", () => {
+  //   if (input.validity.valid) {
+  //     error.style.display = "none";
+  //   } else {
+  //     error.style.display = "block";
+  //   }
+  // });
