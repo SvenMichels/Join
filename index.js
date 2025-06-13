24fb1337910cffafc5de9ac94e1041372b2c196b
Prodegyi <90901987+Prodegyi@users.noreply.github.com>
2025-06-13 20:39:40 +0200

    fixed logo animation in Index

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const bigLogo = document.getElementById("bigLogo");
-  const smallLogo = document.getElementById("logo");
+  const bigLogo = document.querySelector(".bigLogo");
 
-  hideElement(smallLogo);
-  animateBigLogoShrink(bigLogo, () => {
-    removeElement(bigLogo);
-    showElement(smallLogo);
+  if (!bigLogo) return;
+
+  bigLogo.classList.add("shrinkToLogo");
+
+  bigLogo.addEventListener("animationend", () => {
+    bigLogo.remove();
   });
 });
-
-function hideElement(element) {
-  element.hidden = true;
-}
-
-function showElement(element) {
-  element.hidden = false;
-}
-
-function removeElement(element) {
-  element.remove();
-}
-
-function animateBigLogoShrink(element) {
-  setTimeout(() => {
-    element.classList.add("shrinkToLogo");
-  }, 1500);
-
-  element.addEventListener("transitionend", function handleTransitionEnd() {
-    element.removeEventListener("transitionend", handleTransitionEnd);
-  });
-}
