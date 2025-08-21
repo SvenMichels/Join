5a75bf00cde0ff27fdcbbc650e91af3bb7201633
Daniel Riedel <d.riedel.1@web.de>
2025-08-21 18:19:06 +0200

    deleted animation function
    erased handleStartupLogoAnimation() because its not needed. animation is pure CSS based

--- index.js
+++ index.js
@@ -7,22 +7,6 @@
 import { setupMobileDeviceListeners } from "./scripts/utils/mobileUtils.js";
 import { LocalStorageService } from "./scripts/utils/localStorageHelper.js";
 
-/**
- * Handles the animation for the fullscreen startup logo.
- * Shrinks the logo and removes it from the DOM after animation completes.
- * @private
- */
-function handleStartupLogoAnimation() {
-  const mainLogoElement = document.querySelector(".logo-fullscreen");
-  if (!mainLogoElement) return;
-
-  setTimeout(() => { 
-  mainLogoElement.classList.add("shrink");
-  },1000);
-  mainLogoElement.addEventListener("animationend", () => {
-  });
-}
-
 function clearCurrentUser() {
   const currentUser = LocalStorageService.getItem("currentUser");
   if (currentUser) {
@@ -35,7 +19,6 @@ function clearCurrentUser() {
  * and setting up mobile-related event listeners.
  */
 window.addEventListener("DOMContentLoaded", () => {
-  handleStartupLogoAnimation();
   setupMobileDeviceListeners();
   clearCurrentUser();
 });
